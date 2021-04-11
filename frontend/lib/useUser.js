9147b5267c9417229e6b56b02ceb942be370bfb7
Virion <kajko195@tlen.pl>
2021-04-11 12:47:30 +0200

    Add useUser custom hook
    -This hook can confirm user authentication.

--- frontend/lib/useUser.js
+++ frontend/lib/useUser.js
@@ -1,3 +1,21 @@
+import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 
-const CURRENT_USER_QUERY = gql
+const CURRENT_USER_QUERY = gql`
+  query {
+    authenticatedItem {
+      ... on User {
+        id
+        name
+        email
+      }
+    }
+  }
+`;
+
+export function useUser() {
+  const { data } = useQuery(CURRENT_USER_QUERY);
+  return data?.authenticatedItem;
+}
+
+export { CURRENT_USER_QUERY };
